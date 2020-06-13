0c7a4a7090bc4eeb0745782a8a7ec89f7e224d0d
Romulus1039 <54459315+Romulus1039@users.noreply.github.com>
2020-06-12 23:25:30 -0300

    Update customer.dto.ts

--- customer/customer.dto.ts
+++ customer/customer.dto.ts
@@ -1,37 +1,47 @@
-import { Controller, Post, Body, Get, Param, Delete } from '@nestjs/common';
-import { CustomerEntity } from 'src/customer.entity';
-import { CustomerService } from './customer.service';
+import { IsInt, IsString, MaxLength, MinLength, IsDate, IsOptional } from "class-validator";
 
-@Controller('customer')
-export class CustomerController {
+export class CustomerDto {
 
-    constructor(private readonly service: CustomerService) { }
+    @IsInt({ message: 'Id não é um numero inteiro válido!' })
+    id: number;
 
-    @Post()
-    save(@Body() customer: CustomerService) {
-        return this.service.save(customer);
-    }
+    @IsString({message: "name não é uma string válida"})
+    @MinLength(10, {message: "deve ter ao menos 10 caracteres" })
+    @MaxLength(30, {message: "deve ter menos de 30 caracteres" })
+    name: string;
+    @IsString({message: "email não é uma string válida"})
+    @MaxLength(80, {message: "O email deve ter menos de 80 caracteres" })
+    email: string;
 
-    
+    @IsString({message: "phone não é uma string válida"})
+    @MaxLength(20, {message: "O phone deve ter menos de 20 caracteres" })
+    phone: string;
+    @IsOptional()
+    @IsString({message: "CPF não e uma string válida"})
+    @MinLength(14, {message: "O CPF deve ter 14 caracteres, formato xxx.xxx.xxx-xx"})
+    @MaxLength(14, {message: "O CPF deve ter 14 caracteres, formato xxx.xxx.xxx-xx"})
+    cpf: string;
 
-    @Get()
-    findAll() {
-        return this.service.findAll();
-    }
+    @IsOptional()
+    @IsString({message: "dataNascimento não é uma string válida"})
+    @MinLength(10, {message: "O dataNascimento deve ter 10 caracteres, formato DD/MM/AAAA"})
+    @MaxLength(10, {message: "O dataNascimento deve ter 10 caracteres, formato DD/MM/AAAA"})
+    dataNascimento: string;
 
-    @Get(":id")
-    findById(@Param() id: number) {
-        return this.service.findById(id);
-    }
 
-    @Get(":cpf")
-    findByCpf(@Param() cpf: string) {
-        return this.service.findById(cpf);
-    }
+    @IsOptional()
+    @IsString({message: "CEP não é uma string válida"})
+    cep: string;
+    @IsOptional()
+    @IsString({message: "sexo não é uma string válida"})
+    sexo: string;
 
-    @Get(":adess")
-    findByAdress(@Param() adress: string) {
-        return this.service.findById(adress);
-    }
+    @IsOptional()
+    @IsString({message: "rua não é uma string válida"})
+    @MaxLength(50, {message: "A rua deve ter no máximo 50 caracteres"})
+    rua: string;
+    @IsOptional()
+    @IsString({message: "numero não é uma string válida"})
+    numero: string;
 
 }
